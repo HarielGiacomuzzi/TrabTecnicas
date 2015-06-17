b28c84323862feddf0056f08626746a94e40124c
mario.dornelles <mario.dornelles@gmail.com>
2015-06-16 21:02:45 -0300

    Classe leilao iniciada.

---
+++ src/business/Leilao.java
@@ -0,0 +1,13 @@
+package business;
+
+import java.util.Calendar;
+
+public class Leilao {
+
+	private int id;
+	private Usuario usuario;
+	private Calendar dataInicio;
+	private Calendar dataFim;	
+	
+	
+}
