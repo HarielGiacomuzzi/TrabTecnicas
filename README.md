fe8f8d0f693a9f4887a516d90e3dc72cef71e953
Hariel Giacomuzzi <hariel.dias@acad.pucrs.br>
2015-06-16 21:56:32 -0300

    added create table script

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # TrabTecnicas
 O objetivo deste trabalho é consolidar o conhecimento sobre conceitos e construção de sistemas empresariais orientados a objetos em arquiteturas multicamadas através da exploração dos tópicos discutidos na disciplina de Técnicas de Programação.
 
-[Definição](/trabalho_2015_1.pdf)
+[Definição](/trabalho_2015_1.pdf) \n
 [Modelagem Conceitual Banco de Dados](https://drive.draw.io/#G0B9VPTiXLamonbXo1OUhkc05Zb1U)
---
+++ trab_tecnicas.sql
@@ -0,0 +1,52 @@
+create table usuarios(
+  id_usuario int not null,
+  nome VARCHAR2(30) not null,
+  cpf varchar(15),
+  cnpj varchar(20),
+  email varchar2(20),
+  
+  primary key(id_usuario),
+  constraint check_cpf_cnpj check(cpf != null OR cnpj != null) 
+);
+
+create table categorias(
+  id_categoria int not null,
+  desc_categoria varchar2(30),
+  
+  PRIMARY KEY (id_categoria)
+);
+
+create table bens(
+  id_bem int not null,
+  categoria_fk int,
+  desc_breve varchar(30),
+  desc_completa varchar2(30),
+  
+  PRIMARY KEY (id_bem),
+  FOREIGN KEY (categoria_fk) references categorias(id_categoria)
+);
+
+create table leilao(
+  id_leilao int not null,
+  tipo_leilao char not null,
+  tipo_lance char not null,
+  data_inicio date,
+  data_fim date,
+  vendedor_fk int,
+  preco NUMBER(6,2),
+  
+  PRIMARY KEY (id_leilao),
+  FOREIGN KEY (vendedor_fk) references usuarios(id_usuario)
+);
+
+create table lances(
+  id_lance int not null,
+  data_lance date,
+  valor_lance NUMBER(6,2),
+  usuario_fk int,
+  leilao_fk int,
+  
+  PRIMARY KEY(id_lance),
+  FOREIGN KEY (usuario_fk) references usuarios(id_usuario),
+  FOREIGN KEY (leilao_fk) references leilao(id_leilao)
+);
